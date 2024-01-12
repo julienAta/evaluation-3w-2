e5f81c6e206c3fe6828b5d3ea3b54c5fc3aa144a
Atanassov <julienatanassov@delosintelligence.fr>
2024-01-12 16:46:07 +0100

    add task button when no task

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useSelector } from "react-redux";
 import Task from "./Task";
 import Filter from "./Filter";
+import { Link } from "react-router-dom";
 
 function TaskList() {
   const { tasks, priorityFilter, statusFilter, sortOrder } = useSelector(
@@ -22,12 +23,24 @@ function TaskList() {
   }
   return (
     <>
-      <Filter />
       <div className="p-4">
         {filteredTasks.length === 0 ? (
-          <p className="text-center">No tasks found.</p>
+          <>
+            <div className="flex flex-col h-40 items-center ">
+              <p className="text-center">No tasks found.</p>
+              <Link to="/add" className="mt-8 mx-2 hover:text-blue-200">
+                Add your first task
+              </Link>
+            </div>
+          </>
         ) : (
-          filteredTasks.map((task) => <Task key={task.id} task={task} />)
+          filteredTasks.map((task) => (
+            <>
+              <div className="p-4">
+                <Filter /> <Task key={task.id} task={task} />
+              </div>
+            </>
+          ))
         )}
       </div>
     </>
